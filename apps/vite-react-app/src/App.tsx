db6ffca4f73da1aa4b62cda80751073d0eb8ca74
shadow <shadowSun192114@gmail.com>
2023-03-02 23:33:34 +0800

    build 优化

--- apps/vite-react-app/src/App.tsx
+++ apps/vite-react-app/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import reactLogo from './assets/react.svg'
+import { ReactComponent as Logo } from '~/assets/react.svg'
 import './App.css'
 
 function App(): JSX.Element {
@@ -12,7 +13,8 @@ function App(): JSX.Element {
           <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
+          <Logo />
         </a>
       </div>
       <h1>Vite + React</h1>
--- apps/vite-react-app/vite.config.ts
+++ apps/vite-react-app/vite.config.ts
@@ -1,21 +1,31 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import svgr from 'vite-plugin-svgr'
+// import { version as reactV } from 'react'
+// import { version as reactDomV } from 'react-dom'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  esbuild: { jsx: 'automatic' },
+  plugins: [react(), tsconfigPaths(), svgr()],
+  server: {
+    host: true,
+  },
   build: {
     rollupOptions: {
+      // output: {
+      //   manualChunks: {
+      //     'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+      //   },
+      // },
+      external: ['react', 'react-dom'],
       output: {
-        manualChunks: {
-          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
-  },
-  optimizeDeps: {
-    include: ['react/jsx-runtime'],
   },
 })
