df862ca1affa17fc449072910e7f9c918087d406
192114 <shadowSun192114@gmail.com>
2023-03-07 17:52:13 +0800

    temp

--- packages/vite-create/commands/init.ts
+++ packages/vite-create/commands/init.ts
@@ -1,2 +1,20 @@
-const init = 'init'
-console.log(init)
+// import inquirer from 'inquirer'
+// import validate from 'validate-npm-package-name'
+
+// console.log(validate)
+
+// export async function init(projectName: string): Promise<void> {
+//   if (validate(projectName).validForNewPackages) {
+//     return
+//   }
+
+//   if (!projectName.startsWith('@apps/')) {
+//     throw new Error('包名格式不正确 例如：@apps/vite-demo')
+//   }
+
+//   try {
+//     const projectRes = await inquirer.prompt()
+//   } catch (error) {
+//     throw error
+//   }
+// }
