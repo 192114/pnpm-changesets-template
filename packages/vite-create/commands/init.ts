c4a20fb725fb54a33bab52953add2fbcecfb2233
shadow <shadowSun192114@gmail.com>
2023-03-10 23:47:57 +0800

    项目clone

--- packages/vite-create/commands/init.ts
+++ packages/vite-create/commands/init.ts
@@ -2,11 +2,14 @@ import prompts from 'prompts'
 import validate from 'validate-npm-package-name'
 import fs from 'fs-extra'
 import path from 'path'
+import clone from 'git-clone/promise.js'
+import ora from 'ora'
 
 interface IAnswerType {
   projectName: string
   dirname: string
   description: string
+  port: number
 }
 
 function validatePackageName(projectName: string) {
@@ -49,7 +52,7 @@ export async function init(projectName: string, configPath: string): Promise<voi
         message: '项目文件夹名称',
         name: 'dirname',
         validate(value: string) {
-          if (!fs.pathExistsSync(path.resolve(process.cwd(), './apps/', value))) {
+          if (fs.pathExistsSync(path.resolve(process.cwd(), './apps/', value))) {
             return '当前目录已存在'
           }
           return true
@@ -66,6 +69,22 @@ export async function init(projectName: string, configPath: string): Promise<voi
           return true
         },
       },
+      {
+        type: 'number',
+        message: '项目启动端口号',
+        name: 'port',
+        validate(value: number) {
+          if (value <= 5172 || value >= 8080) {
+            return '端口限制在5172 - 8080 之间！'
+          }
+
+          if (appListJson.findIndex(item => item.port === value) >= 0) {
+            return '端口号已被占用！'
+          }
+
+          return true
+        },
+      },
     ],
     {
       onCancel() {
@@ -77,13 +96,31 @@ export async function init(projectName: string, configPath: string): Promise<voi
   )
 
   // 处理中止的情况
-  if (!projectRes.projectName || !projectRes.dirname || !projectRes.description) {
+  if (
+    !projectRes.projectName ||
+    !projectRes.dirname ||
+    !projectRes.description ||
+    !projectRes.port
+  ) {
     return
   }
+
+  // 创建文件夹
+  const targetDir = path.resolve(process.cwd(), './apps/', projectRes.dirname)
+  const spinForCreateDir = ora(`创建${targetDir}目录`).start()
+  await fs.ensureDir(path.resolve(process.cwd(), './apps/', projectRes.dirname))
+  spinForCreateDir.succeed('目录创建成功！')
+  // 从github上下载工程
+  const spinCloneTemplate = ora('模版下载中').start()
+  await clone(
+    'https://github.com/192114/vite-react-ts-template-for-monorepo.git',
+    path.resolve(process.cwd(), './apps/', projectRes.dirname)
+  )
+  spinCloneTemplate.succeed('模版下载成功！')
+  // 修改模版
+  // handlebars 修改模版
 
   appListJson.push(projectRes)
 
   await fs.writeJSON(configPath, appListJson)
-
-  await fs.ensureDir(path.resolve(process.cwd(), './apps/', projectRes.dirname))
 }
