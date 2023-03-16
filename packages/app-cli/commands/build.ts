181e80fae084e49183830aa102290b54404573d8
192114 <shadowSun192114@gmail.com>
2023-03-16 17:39:35 +0800

    temp

---
+++ packages/app-cli/commands/build.ts
@@ -0,0 +1,8 @@
+// import shell from 'shelljs'
+
+// export async function build(projectName: string, configPath: string): Promise<void> {
+//   if (projectName) {
+//     // 清理对应项目的包
+//     shell.exec(`pnpm --filter ${projectName} run build`)
+//   }
+// }
